93df5b748c8bdb786edef5f23a74e9de0c9cb2e5
Yeviyen <91231290+Yeviyen@users.noreply.github.com>
2021-11-22 21:35:45 +0800

    moviee8

---
+++ movie8/js/sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "pwebmovie";
+var urlsToCache = [
+    "/",
+    "/js/tmdb.js",
+    "/js/materialize.min.js",
+    "/js/init.js",
+    "/js/api.js",
+    "/css/materialize.min.css",
+    "/img/movie1080.jpg",
+    "/img/movie720.jpg",
+    "/img/movie480.jpg",
+    "/img/movie1600.jpg",
+    "/css/style.css ",
+];
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches
+        .match(event.request, {
+            cacheName: CACHE_NAME
+        })
+        .then(function (response) {
+            if (response) {
+                console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+                return response;
+            }
+            console.log(
+                "ServiceWorker: Memuat aset dari server: ",
+                event.request.url
+            );
+            return fetch(event.request);
+        })
+    );
+});
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName != CACHE_NAME) {
+                        console.log("ServiceWorker: cache " + cacheName + " dihapus");
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
---
+++ movie8/js/tmdb.js
@@ -0,0 +1,42 @@
+const key = 'e0690b8ffa5e3000335d93f7813ed515';
+const endpoint_url = 'https://api.themoviedb.org/3/';
+
+
+
+function getListMovie(services, sectiontitle) {
+    // fetch untuk dapat data dari server API penyedia
+    fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
+    .then(status)
+    .then(json)
+    .then(function(data) {
+    // Objek/array JavaScript dari response.json() masuk lewat data.
+    // Menyusun komponen card movie secara dinamis
+
+    var moviesHTML = "";
+    data.results.forEach(function(movie) {
+    moviesHTML =  moviesHTML + `
+        <div class="col m3 s6">
+            <div class="card moviecard">
+                <a href="./movie.html?id=${movie.id}">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
+                    </div>
+                </a>
+                <div class="card-content text-center">
+                    <strong>${movie.title}</strong><br>
+                    <strong>Release Date : ${movie.release_date}</strong>
+                </div>
+
+            </div>
+        </div>
+        `;
+    });
+
+    document.getElementById("movie_list").innerHTML = moviesHTML;
+    document.getElementById("section_title").innerHTML = sectiontitle;
+
+    })
+    .catch(error);
+}
+
+
